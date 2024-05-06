9302c16050d70058fce7b19abbd2ee06f3eabf68
Wronskode <142849734+Wronskode@users.noreply.github.com>
2024-05-06 18:39:12 +0200

    [SG-55] Game

--- src/main/java/fr/projet/ia/WinnerStrat.java
+++ src/main/java/fr/projet/ia/WinnerStrat.java
@@ -6,16 +6,22 @@ import fr.projet.graph.Graph;
 import fr.projet.graph.Vertex;
 import javafx.util.Pair;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class WinnerStrat extends InterfaceIA {
     private List<Graph> stratGagnante;
     private Set<Pair<Vertex, Vertex>> toCut;
+    private Graph T1;
+    private Graph T2;
+
     public WinnerStrat(Game game, Turn plays, List<Graph> stratGagnante) {
         super(game, plays);
         this.stratGagnante = stratGagnante;
         this.toCut = stratGagnante.getFirst().getNeighbors();
+        this.T1 = stratGagnante.getFirst();
+        this.T2 = stratGagnante.get(1);
     }
 
     public Pair<Vertex, Vertex> playCUT() {
@@ -35,7 +41,50 @@ public class WinnerStrat extends InterfaceIA {
         return null;
     }
 
+    public static Graph modEgdeGraph (Graph g, HashSet<Pair<Vertex,Vertex>> edgesAdd, HashSet<Pair<Vertex,Vertex>> edgesDel) {
+        for (Pair<Vertex,Vertex> e : edgesAdd) {
+            g.addNeighbor(e);
+        }
+        for (Pair<Vertex,Vertex> e : edgesDel) {
+            if (g.getNeighbors().contains(e)) {
+                g.removeNeighbor(e);
+            }
+            if (g.getNeighbors().contains(Graph.reverseEdge(e))) {
+                g.removeNeighbor(Graph.reverseEdge(e));
+            }
+        }
+        return g;
+    }
+
     public Pair<Vertex, Vertex> playSHORT() {
+        HashSet<Pair<Vertex, Vertex>> securedInit = new HashSet<>(game.getSecured());
+        HashSet<Pair<Vertex, Vertex>> cuttedInit = new HashSet<>(game.getCutted());
+        Graph T1mod = modEgdeGraph(T1.copy(),securedInit,cuttedInit);
+        Graph T2mod = modEgdeGraph(T2.copy(),securedInit,cuttedInit);
+        if (T1mod.estConnexe() && T2mod.estConnexe()) {
+            for (Pair<Vertex,Vertex> e : graph.getNeighbors()) {
+                if (!securedInit.contains(e) && !cuttedInit.contains(e) && !game.getCutted().contains(e) && !game.getSecured().contains(e)){
+                    return e;
+                }
+            }
+        }
+        if (!T1mod.estConnexe())
+            for (Pair<Vertex,Vertex> e : T2mod.getNeighbors()) {
+                T1mod.addNeighbor(e);
+                if (T1mod.estConnexe() && !game.getCutted().contains(e) && !game.getSecured().contains(e)) {
+                    return e;
+                }
+                T1mod.removeNeighbor(e);
+            }
+        if (!T2mod.estConnexe())
+            for (Pair<Vertex,Vertex> e : T1mod.getNeighbors()) {
+                T2mod.addNeighbor(e);
+                if (T2mod.estConnexe() && !game.getCutted().contains(e) && !game.getSecured().contains(e)) {
+                    return e;
+                }
+                T2mod.removeNeighbor(e);
+            }
+        System.out.println("Erreur : pas d'arête à couper");
         return null;
     }
 }
